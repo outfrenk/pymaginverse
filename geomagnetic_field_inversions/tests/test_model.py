7ee6aa60eea160c182d2d269aef7e566c93616f3
Out <f.out@uu.nl>
2023-11-03 22:01:32 +0100

    Update test_model.py

--- geomagnetic_field_inversions/tests/test_model.py
+++ geomagnetic_field_inversions/tests/test_model.py
@@ -29,6 +29,7 @@ def read_igrf(degree: int) -> (np.ndarray, np.ndarray):
 
 
 @pytest.mark.parametrize("maxdegree, types, max_iter", SCENARIOS)
+# TODO: fix pytest
 def test_model(maxdegree: int,
                types: list,
                max_iter: int,
@@ -67,22 +68,21 @@ def test_model(maxdegree: int,
     world_coord[:, 2] = 6371.2
     # generate linear observations around the globe
     frechxyz = frechet.frechet_basis(world_coord, maxdegree)
-    forw_obs = fwtools.forward_obs(igrfcoef, frechxyz)
-    forw_obs[5::7] = np.degrees(forw_obs[5::7])
-    forw_obs[6::7] = np.degrees(forw_obs[6::7])
+    forw_obs = np.zeros((len(igrfcoef), 7, len(world_coord)))
+    for t, coef in enumerate(igrfcoef):
+        forw_obs[t] = fwtools.forward_obs(coef[np.newaxis], frechxyz)
+    forw_obs[:, 5:] = np.degrees(forw_obs[:, 5:])
     # use class
-    testclass = FieldInversion(time_array=time_array, maxdegree=maxdegree,
-                               geodetic=False)
+    testclass = FieldInversion(time_array=time_array, maxdegree=maxdegree)
     for i, loc in enumerate(world_coord):
         name = f'{world_coord[i]}'
         temp = StationData(lat=latgrid[i], lon=longrid[i], name=name)
         for typ in types:
             timedata = np.zeros((2, len(time_array)))
             timedata[0] = time_array
-            timedata[1] = forw_obs[typ+i*7]
+            timedata[1] = forw_obs[:, typ, i]
             temp.add_data(datatypes[typ], data=timedata,
                           time_factor=1, error=[errortypes[typ]])
-        temp.fitting()
         testclass.add_data(temp)
     testclass.prepare_inversion()
     x0 = np.zeros(testclass._nm_total)
