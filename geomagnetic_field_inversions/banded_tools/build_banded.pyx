d48687e3fa576ef346a0990702b304fd6689cb27
Max Schanner <arthus@gfz-potsdam.de>
2024-03-07 15:32:15 +0100

    Revert loop order, add experimental direct loop.

--- geomagnetic_field_inversions/banded_tools/build_banded.pyx
+++ geomagnetic_field_inversions/banded_tools/build_banded.pyx
@@ -3,6 +3,8 @@
 # python setup.py build_ext --inplace
 
 from cython.parallel import prange
+from cpython.mem cimport PyMem_Malloc, PyMem_Free
+from libc.math cimport abs
 
 import numpy as np
 
@@ -58,6 +60,46 @@ def build_banded_2(
     )
     cdef int k, it_t, it_s, jt_t, jt_s, it, jt, kt, ind
 
+
+    for it in prange(bandw, nogil=True):
+        for jt in range(n_t*n_coeffs-it):
+            k = bandw-it-1
+            it_t = (it + jt) // n_coeffs
+            jt_t = jt // n_coeffs
+
+            it_s = (it + jt) % n_coeffs
+            jt_s = jt % n_coeffs
+            ind = it_t * n_t + jt_t
+
+            for kt in range(starts[ind], starts[ind+1]):
+                banded[k, it + jt] += (
+                    temporal[it_t, nonzero_inds[kt]]
+                    * base_DIF[it_s, nonzero_inds[kt]]
+                    * temporal[jt_t, nonzero_inds[kt]]
+                    * base_DIF[jt_s, nonzero_inds[kt]]
+                )
+
+    return banded
+
+
+def build_banded_2_direct(
+    double[:, ::1] base_DIF,
+    double[:, ::1] temporal,
+    int p,
+    int[::1] lookup_inds,
+    int[::1] lookup_starts,
+):
+    # Calculate the normal equations matrix by using the precalculated
+    # nonzero indices to speed up the loops below
+    cdef int n_t = temporal.shape[0]
+    cdef int n_coeffs = base_DIF.shape[0]
+    cdef int bandw = (p + 1) * n_coeffs + 1
+    cdef double[:, ::1] banded = np.zeros(
+        (bandw, n_t*n_coeffs),
+        dtype=np.float64,
+    )
+    cdef int k, it_t, it_s, jt_t, jt_s, it, jt, kt, lt
+
     for jt in prange(n_t*n_coeffs, nogil=True):
         # for jt in range(n_t*n_coeffs-it):
         for it in range(bandw):
@@ -74,17 +116,19 @@ def build_banded_2(
 
             it_s = (it + jt) % n_coeffs
             jt_s = jt % n_coeffs
-            ind = it_t * n_t + jt_t
 
-            for kt in range(starts[ind], starts[ind+1]):
-                banded[k, it + jt] += (
-                    temporal[it_t, nonzero_inds[kt]]
-                    * base_DIF[it_s, nonzero_inds[kt]]
-                    * temporal[jt_t, nonzero_inds[kt]]
-                    * base_DIF[jt_s, nonzero_inds[kt]]
-                )
+            for kt in range(lookup_starts[it_t], lookup_starts[it_t+1]):
+                for lt in range(lookup_starts[jt_t], lookup_starts[jt_t+1]):
+                    if (lookup_inds[kt] - lookup_inds[lt]) == 0:
+                        banded[k, it + jt] += (
+                            temporal[it_t, lookup_inds[kt]]
+                            * base_DIF[it_s, lookup_inds[kt]]
+                            * temporal[jt_t, lookup_inds[kt]]
+                            * base_DIF[jt_s, lookup_inds[kt]]
+                        )
 
     return banded
+
 
 
 def build_banded_3(
